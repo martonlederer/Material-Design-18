d8c97e9f393eeb286768f0af483a3d1dc7532822
Marton <martondeveloper@gmail.com>
2018-10-27 21:25:36 +0200

    Sidenav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,28 @@
+var navOpened = false;
+
 $(document).ready(function() {
 
   Waves.init();
   Waves.attach('.waves-effect', ['waves-button', 'waves-float']);
+
+  $("#menuTrigger").click(function() {
+
+    if(navOpened) {
+
+      $("#sidenav").css("width", "0");
+      $("#main").css("margin-left", "0");
+
+      navOpened = false;
+
+    }else {
+
+      $("#sidenav").css("width", "250px");
+      $("#main").css("margin-left", "250px");
+
+      navOpened = true;
+
+    }
+
+  });
 
 });
