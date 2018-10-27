1b141fcb50c90fabe8143dc29376ac8a22b12563
Marton <martondeveloper@gmail.com>
2018-10-27 21:56:22 +0200

    Menu trigger animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,18 @@ $(document).ready(function() {
       $("#sidenav").css("width", "0");
       $("#main").css("margin-left", "0");
 
+      $("#menuTrigger .close").fadeOut();
+      $("#menuTrigger .menu").fadeIn();
+
       navOpened = false;
 
     }else {
 
       $("#sidenav").css("width", "250px");
       $("#main").css("margin-left", "250px");
+
+      $("#menuTrigger .menu").fadeOut();
+      $("#menuTrigger .close").fadeIn();
 
       navOpened = true;
 
