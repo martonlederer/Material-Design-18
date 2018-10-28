f38a4cc02516ced3007a50b8d7c171da6e81c8e7
Marton <martondeveloper@gmail.com>
2018-10-28 15:55:28 +0100

    Basic text fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,37 @@ $(document).ready(function() {
   });
 
 });
+
+jQuery(function($) {
+
+  $('.field-input').focus(function() {
+
+    $(this).parent().addClass('is-focused has-label');
+
+  });
+
+  $('.field-input').each(function() {
+
+    if($(this).val() != '') {
+
+      $(this).parent().addClass('has-label');
+
+    }
+
+  });
+
+  $('.field-input').blur(function() {
+
+    $parent = $(this).parent();
+
+    if($(this).val() == '') {
+
+      $parent.removeClass('has-label');
+
+    }
+
+    $parent.removeClass('is-focused');
+
+  });
+
+})
